59b4d210ae11b365256700f99ba7108e483b05bf
Ola Vea <ola@olavea.com>
2019-05-16 07:14:50 +0200

    title + icon minus audio file because of size

---
+++ build/precache-manifest.459f0808469ff72908d3325049dbd4d1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b44017a5bab0787fbdf958dfb4442eb3",
+    "url": "/static/media/lille-my.b44017a5.mp3"
+  },
+  {
+    "revision": "eaee3236b1e701579bed756a46f7e505",
+    "url": "/static/media/icons8-resume-button.eaee3236.svg"
+  },
+  {
+    "revision": "ad582e760160b2fb2e6d3ad2b14610cb",
+    "url": "/static/media/icons8-play.ad582e76.svg"
+  },
+  {
+    "revision": "9715ab345e89e1e6532bf2ef8d0fdc18",
+    "url": "/static/media/icons8-pause.9715ab34.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9a5b4a5bcf31b3e010dc",
+    "url": "/static/js/main.9a5b4a5b.chunk.js"
+  },
+  {
+    "revision": "dda13a01b156ad275fe9",
+    "url": "/static/js/1.dda13a01.chunk.js"
+  },
+  {
+    "revision": "9a5b4a5bcf31b3e010dc",
+    "url": "/static/css/main.be9e945a.chunk.css"
+  },
+  {
+    "revision": "754fc133bdd9596d85f28eb6ae964f77",
+    "url": "/index.html"
+  }
+];
